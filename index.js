7e744903f4757e9c47f2654aa6def2105e3a8008
Christine Bussinger <christinebussingerdev@gmail.com>
2020-04-21 19:47:46 -0400

    added project router, configged new action router in server, added action router file

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ require('dotenv').config()
 const server = require('./server')
 
 
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
 
 server.listen(port, () => {
--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require('express')
 
 // router imports
 
+const projectRouter = require('./routers/projectRouter')
+
 const server = express();
 
 server.use(express.json())
 
 // use routers
+
+server.use('/projects', projectRouter)
+
 
 module.exports = server
